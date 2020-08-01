258af9bfdd682e281db471400abe6c15f394b98d
Shubham Suri <shubham013@live.com>
2020-08-01 20:02:01 -0300

    disable sequalize logging

--- server/util/database.js
+++ server/util/database.js
@@ -11,20 +11,22 @@ const DB_USER = process.env.DB_USER || 'jrana'
 const DB_PASSWORD = process.env.DB_PASSWORD || 'jrana'
 const DB_HOST = process.env.DB_HOST || '54.86.125.185'
 
-
-const options = {
-  define: {
-      //prevent pluralization of table names
-      freezeTableName: true,
-      timestamps: false
-  }
-}
+// wrongly used : to be removed -- notify aman
+// const options = {
+//   define: {
+//       //prevent pluralization of table names
+//       freezeTableName: true,
+//       timestamps: false
+//   },
+//   logging: false
+// }
 
 // declare parameteres for interaction with mysql database
 const sequelize = new Sequelize(SCHEMA, DB_USER, DB_PASSWORD, {
   dialect: 'mysql',
   host: DB_HOST,
   port:3306,
-}, options);
+  logging: false
+});
 
 module.exports = sequelize;
